7e9992e5d09ee559c41cba6e8dd3b0714d01c199
Abhijeet M <34742262+abhivik@users.noreply.github.com>
2023-07-26 18:16:29 +0530

    Update 4.1 Heads or Tails.py

--- 4.1 Heads or Tails.py
+++ 4.1 Heads or Tails.py
@@ -1,13 +1,10 @@
-## Banker Roulette ##
-# Write a program that will select a random name from list of names.
-# The person selected will have to pay for everybody.
-
-import random
-
-name_list = input("Enter the names seperated by comma: ")
-names = name_list.split(", ")
-
-total_names = len(names)
-random_name = random.randint(0, total_names - 1)
-person_paying = names[random_name]
-print(f"{person_paying} is going to buy the meal today")
+#Remember to use the random module
+#Hint: Remember to import the random module here at the top of the file. 🎲
+import random 
+#Write the rest of your code below this line 👇
+coin = 0,1
+toss = random.choice(coin)
+if toss == 0:
+    print("Tails")
+elif toss == 1:
+    print("Heads")
