be6eda270ac6ebfd4c3cc3390a2a22b2a3b5bc61
Abhijeet M <34742262+abhivik@users.noreply.github.com>
2023-07-26 18:18:57 +0530

    Update 3.6 Treasure Island.py

--- 3.6 Treasure Island.py
+++ 3.6 Treasure Island.py
@@ -1,10 +1,47 @@
-#Remember to use the random module
-#Hint: Remember to import the random module here at the top of the file. 🎲
-import random 
-#Write the rest of your code below this line 👇
-coin = 0,1
-toss = random.choice(coin)
-if toss == 0:
-    print("Tails")
-elif toss == 1:
-    print("Heads")
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+step1 = input("Which direction you want to go? Left or Right?: ")
+step1 = step1.lower()
+if step1 == "left":
+    step2= input("Do you want to swim or wait?: ")
+    step2 = step2.lower()
+    if step2 == "wait":
+        step3 = input("Which coloured door you want to enter through? Red or Blue?: ")
+        step3 = step3.lower()
+        if step3 == "blue":
+            print("You were eaten by beasts. Game Over!")
+        elif step3 == "red":
+            print("You were burned by fire. Game Over!")
+        elif step3 == "yellow":
+            print("You Win!")
+        else:
+            print("Game")
+    else:
+        print("You were attacked by trout. Game Over!")
+else: 
+    print("You fell into a hole. Game Over!")   
