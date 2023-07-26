f516f49236a2913b5557661aa86d71bb8a33db7b
Abhijeet M <34742262+abhivik@users.noreply.github.com>
2023-07-26 18:01:27 +0530

    Update 2.3 Tip Calculator.py

--- 2.3 Tip Calculator.py
+++ 2.3 Tip Calculator.py
@@ -1,6 +1,7 @@
-#Tip Calculator
-#If the bill was $150.00, split between 5 people, with 12% tip. 
-#Each person should pay (150.00 / 5) * 1.12 = 33.6
+## Tip Calculator ##
+# Split a bill between individuals with included tip of 5%, 12%, and 15% included.
+# If the bill was $150.00, split between 5 people, with 12% tip. 
+# Each person should pay (150.00 / 5) * 1.12 = 33.6
 
 print("Welcome to the tip calculator.")
 
