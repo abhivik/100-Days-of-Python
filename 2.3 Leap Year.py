6214347599b8cc61cb4e8d27e3643f8e0d4d0ab4
Abhijeet M <34742262+abhivik@users.noreply.github.com>
2023-07-26 17:57:02 +0530

    Update 2.3 Leap Year.py

--- 2.3 Leap Year.py
+++ 2.3 Leap Year.py
@@ -8,9 +8,19 @@
      # **unless** the year is also evenly divisible by 400
 
 # Explaination:
-# Leap Year - (1) If year is divisible by 4 and not divisible 100.
-#             (2)  If year is divisible by 4 and 100
+# Leap Year - (1) Every year that is evenly divisible by 4 
+#             (2) **except** every year that is evenly divisible by 100 
+#             (3) **unless** the year is also evenly divisible by 400
 
+# e.g. The year 2000:
+# 2000 ÷ 4 = 500 (Leap)
+# 2000 ÷ 100 = 20 (Not Leap)
+# 2000 ÷ 400 = 5 (Leap!)
+# So the year 2000 is a leap year.
+# But the year 2100 is not a leap year because:
+# 2100 ÷ 4 = 525 (Leap)
+# 2100 ÷ 100 = 21 (Not Leap)
+# 2100 ÷ 400 = 5.25 (Not Leap)
 
 year =int(input("Which year do you want to check? "))
 
