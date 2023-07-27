522b50a086d80a19a2252cc574d2469b23260b25
Abhijeet M <34742262+abhivik@users.noreply.github.com>
2023-07-27 21:43:19 +0530

    Update 5.1 Average Height using for loop.py

--- 5.1 Average Height using for loop.py
+++ 5.1 Average Height using for loop.py
@@ -11,9 +11,9 @@ total_height = 0
 for height in student_heights:
     total_height += height
 #print(total_height)
-    count = 0
-    for number in student_heights:
-        count += 1
+count = 0
+for number in student_heights:
+    count += 1
     #print(count)
 print(round(total_height/count))
 
