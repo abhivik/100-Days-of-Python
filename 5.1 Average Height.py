87ef8b104eeddca8d9ce4dce5962aaa5843a0b1c
Abhijeet M <34742262+abhivik@users.noreply.github.com>
2023-07-27 21:37:03 +0530

    Add files via upload

---
+++ 5.1 Average Height.py
@@ -0,0 +1,19 @@
+## Average Height: Using for loop ##
+
+# write a program that calculates the average student height from a List of heights.
+
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+#print(student_heights)
+
+total_height = 0
+for height in student_heights:
+    total_height += height
+#print(total_height)
+    count = 0
+    for number in student_heights:
+        count += 1
+    #print(count)
+print(round(total_height/count))
+
